717069279f0ff12a1109b7ec0c0e1a3f306d60b0
AhmadNader319 <anader3194747@gmail.com>
2024-09-22 14:34:08 +0300

    Applying 3-tier architecture and OOP concepts, Creating the main classes and folders structure

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,9 @@ import java.util.logging.FileHandler;
 
 
 public class Main {
-    static FileHandler fileHandler;
-    static FileReader fileReader;
-    static BufferedReader bufferedReader;
-    static FileHandler fileWriter;
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        String[] pathFuntions = new String[3];
-        ArrayList<String> arraysProblems;
-        ArrayList<String> listProblems;
-        ArrayList<String> OOPProblems;
+    }
 
-    }
-    public static ArrayList<String> getArrayProblems(String path) throws IOException {
-        ArrayList<String> problems = new ArrayList<>();
-        fileReader = new FileReader(path);
-        return problems;
-    }
 }
---
+++ src/Utils/DBUtils.java
@@ -0,0 +1,11 @@
+package Utils;
+
+import java.sql.Connection;
+
+public class DBUtils {
+
+    public static void getConnection(){
+    }
+    public static void closeConnection(Connection conn){
+    }
+}
