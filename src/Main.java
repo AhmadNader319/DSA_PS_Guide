f9809bc7578eb34a37397845b5253e8fcb36cca5
Ahmad Nader <anader3194747@gmail.com>
2024-10-14 14:30:37 +0300

    Solving basic recursion problems

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.util.logging.FileHandler;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Stack<Character> stack = new Stack<Character>();
-
+        int[] arr = {2,1,3};
+        System.out.println(findMax(arr,arr.length-1));
     }
     //General Problems on Recursion
     //Factorial of Number
@@ -21,5 +21,38 @@ public class Main {
             return n*factorial(n-1);
         }
     }
+    //Sum of digit of a number
+    public static int sumOfDigits(int number){
+        if (number == 0){
+            return 0;
+        }
+        return number%10 + sumOfDigits(number/10);
+    }
+    //fibonacci
+    public static int fibonacci(int n)  {
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
     //
+    public static String reverseString(String str){
+        if (str.isEmpty()||str.length()==1)
+            return str;
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+    public static int powerOfNumber(int x, int n){
+        if (n==0)
+            return 1;
+        return x * powerOfNumber(x, n-1);
+    }
+    public static int findMax(int[] arr, int index) {
+        if (index == 0){
+            return arr[0];
+        }
+        int value = findMax(arr, index-1);
+        return Math.max(arr[index], value);
+    }
 }
